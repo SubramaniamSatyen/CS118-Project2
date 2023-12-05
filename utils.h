9e318d2f069dd50d34195f213aa3901cff0a7c14
Satyen Subramaniam <satyensubramaniam@gmail.com>
2023-12-05 14:24:06 -0800

    Cleaning code

--- utils.h
+++ utils.h
@@ -13,8 +13,7 @@
 #define CLIENT_PORT_TO 5001
 #define PAYLOAD_SIZE 1024
 #define WINDOW_SIZE 20
-#define TIMEOUT 0.3               // Determine this from fixed prop delay of 1 ms
-#define MAX_SEQUENCE 1024
+#define TIMEOUT 0.22               // Determine this from fixed prop delay of 1 ms
 
 //Our macros
 #define MAX_PACKET_SIZE 1200
@@ -25,63 +24,8 @@
 #define MAX_WINDOW_SIZE 100
 #define RECIEVE_WINDOW_SIZE (2*MAX_WINDOW_SIZE)
 #define CLOSE_PACKET_NUM 255
-#define CLOSE_MULTI 5
+#define CLOSE_MULTI 2
 
 #define LOGGING_ENABLED false
 
-
-// Packet Layout
-// You may change this if you want to
-struct packet {
-    unsigned short seqnum;
-    unsigned short acknum;
-    char ack;
-    char last;
-    unsigned int length;
-    char payload[PAYLOAD_SIZE];
-};
-
-// Utility function to build a packet
-void build_packet(struct packet* pkt, unsigned short seqnum, unsigned short acknum, char last, char ack,unsigned int length, const char* payload) {
-    pkt->seqnum = seqnum;
-    pkt->acknum = acknum;
-    pkt->ack = ack;
-    pkt->last = last;
-    pkt->length = length;
-    memcpy(pkt->payload, payload, length);
-}
-
-// Utility function to print a packet
-void printRecv(struct packet* pkt) {
-    printf("RECV %d %d%s%s\n", pkt->seqnum, pkt->acknum, pkt->last ? " LAST": "", (pkt->ack) ? " ACK": "");
-}
-
-void printSend(struct packet* pkt, int resend) {
-    if (resend)
-        printf("RESEND %d %d%s%s\n", pkt->seqnum, pkt->acknum, pkt->last ? " LAST": "", pkt->ack ? " ACK": "");
-    else
-        printf("SEND %d %d%s%s\n", pkt->seqnum, pkt->acknum, pkt->last ? " LAST": "", pkt->ack ? " ACK": "");
-}
-
-//returns the number of packets between end and start
-uint8_t seqdiff(uint8_t start, uint8_t end){
-    if (end < start){end += RECIEVE_WINDOW_SIZE;}
-    return end - start; 
-}
-
-bool inrange(uint8_t start, uint8_t index, uint8_t cwnd){
-    if (index < start){
-        index += RECIEVE_WINDOW_SIZE;
-    }
-    return cwnd > (index - start);
-}
-
-bool isEnd(uint8_t seq, uint8_t final){
-    switch (final){
-        case CLOSE_PACKET_NUM:
-            return false;
-        default: 
-            return (seq-1) == final; 
-    }
-}
 #endif
